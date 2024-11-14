310804b894a404e9b7959ce7ebd2b7630dbc3925
hokwach <hezronokwach@gmail.com>
2024-11-14 19:38:40 +0300

    maintain the position of placeholders after sending a message

--- js/main.js
+++ js/main.js
@@ -278,14 +278,28 @@ var owlSingleSlider = function () {
 }
 
 var floatingLabel = function () {
-	$('.form-control').on('input', function() {
-	  var $field = $(this).closest('.form-group');
-	  if (this.value) {
-	    $field.addClass('field--not-empty');
-	  } else {
-	    $field.removeClass('field--not-empty');
-	  }
-	});
+    $('.form-control').on('input', function() {
+        var $field = $(this).closest('.form-group');
+        if (this.value) {
+            $field.addClass('field--not-empty');
+        } else {
+            $field.removeClass('field--not-empty');
+        }
+    });
+
+    // Add this new event listener
+    $('.form-control').on('focus', function() {
+        var $field = $(this).closest('.form-group');
+        $field.addClass('field--not-empty');
+    });
+
+    // Add this new event listener
+    $('.form-control').on('blur', function() {
+        var $field = $(this).closest('.form-group');
+        if (!this.value) {
+            $field.removeClass('field--not-empty');
+        }
+    });
 };
 
 
@@ -561,67 +575,7 @@ var jarallaxPlugin = function() {
 	});
 };
 
-// var contactForm = function() {
-// 	if ($('#contactForm').length > 0 ) {
-// 		$( "#contactForm" ).validate( {
-// 			rules: {
-// 				name: "required",
-// 				email: {
-// 					required: true,
-// 					email: true
-// 				},
-// 				message: {
-// 					required: true,
-// 					minlength: 5
-// 				}
-// 			},
-// 			messages: {
-// 				name: "Please enter your name",
-// 				email: "Please enter a valid email address",
-// 				message: "Please enter a message"
-// 			},
-// 			errorElement: 'span',
-// 			errorLabelContainer: '.form-error',
-// 			/* submit via ajax */
-// 			submitHandler: function(form) {		
-// 				var $submit = $('.submitting'),
-// 					waitText = 'Submitting...';
 
-// 				$.ajax({   	
-// 			      type: "POST",
-// 			      url: "php/send-email.php",
-// 			      data: $(form).serialize(),
-
-// 			      beforeSend: function() { 
-// 			      	$submit.css('display', 'block').text(waitText);
-// 			      },
-// 			      success: function(msg) {
-// 	               if (msg == 'OK') {
-// 	               	$('#form-message-warning').hide();
-// 			            setTimeout(function(){
-// 	               		$('#contactForm').fadeOut();
-// 	               	}, 1000);
-// 			            setTimeout(function(){
-// 			               $('#form-message-success').fadeIn();   
-// 	               	}, 1400);
-		               
-// 		            } else {
-// 		               $('#form-message-warning').html(msg);
-// 			            $('#form-message-warning').fadeIn();
-// 			            $submit.css('display', 'none');
-// 		            }
-// 			      },
-// 			      error: function() {
-// 			      	$('#form-message-warning').html("Something went wrong. Please try again.");
-// 			         $('#form-message-warning').fadeIn();
-// 			         $submit.css('display', 'none');
-// 			      }
-// 		      });    		
-// 	  		}
-			
-// 		} );
-// 	}
-// };
 
 var stickyFillPlugin = function() {
 	var elements = document.querySelectorAll('.unslate_co--sticky');
